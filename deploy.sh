37abe9f032424e622c61466fa8b876f9a8dd8989
MonsterXiong <942849672@qq.com>
2021-03-31 23:40:55 +0800

    init

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+  
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+#创建.nojekyll 防止Github Pages build错误
+touch .nojekyll
+
+git init
+git add -A
+git commit -m 'deploy'
+git remote add origin https://github.com/MonsterXiong/blog.git
+ git push -f git@github.com:<MonsterXiong>/blog.git master:gh-pages
+
+cd -
