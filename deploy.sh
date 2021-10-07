e5e9dbb36bae551e1a028a1a363d46e4cb99ec8e
monster-liu <liuxiongcheng9774@gmail.com>
2021-10-07 08:29:59 +0800

    fix: update white

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ vuepress build
 cd .vuepress/dist
 
 # 如果是发布到自定义域名
-# echo 'blog.monsterbear.top' > CNAME
+echo 'blog.monsterbear.top' > CNAME
 
 git init
 git config user.name "MonsterXiong"
@@ -27,4 +27,6 @@ git commit -m 'deploy'
 # git push -f https://github.com/MonsterXiong/blog.git master:gh-pages
 git push -f "https://${TOKEN}@github.com/MonsterXiong/blog.git" "master:gh-pages"
 
-cd -
+# Cleanup
+cd ../..
+rm -rf .vuepress/dist
