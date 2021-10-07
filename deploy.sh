2ae298beed9c371815f77debea7a2b9b98701e07
monster-liu <liuxiongcheng9774@gmail.com>
2021-10-07 08:54:05 +0800

    fix: update white

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@
 set -e
 
 # 生成静态文件
-# npm run build
-vuepress build
+npm run build
+# vuepress build
 
 # 进入生成的文件夹
-cd .vuepress/dist
+cd docs/.vuepress/dist
 # cd .vuepress/dist
 
 # 如果是发布到自定义域名
@@ -18,15 +18,17 @@ git init
 git config user.name "MonsterXiong"
 git config user.email "942849672@qq.com"
 git add -A
-git commit -m 'deploy'
+# git commit -m 'deploy'
+git commit -m 'feat: deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f https://github.com/MonsterXiong/blog.git master:gh-pages
-git push -f "https://${TOKEN}@github.com/MonsterXiong/blog.git" "master:gh-pages"
+git push -f https://${TOKEN}@github.com/MonsterXiong/blog.git master:gh-pages
 
 # Cleanup
-cd ../..
-rm -rf .vuepress/dist
+cd -
+# cd ../..
+# rm -rf .vuepress/dist
